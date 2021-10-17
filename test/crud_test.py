ffc3147dc9b180a277aae4c901fdb8421d586693
Lee <lee.m.everett@gmail.com>
2021-10-17 16:57:32 +0100

    init

---
+++ test/crud_test.py
@@ -0,0 +1,42 @@
+import pytest
+from dynaconf import settings
+
+from app.crud.crud import create_user, get_users
+from app.db import models
+from app.db.database import SessionLocal, engine
+from app.schemas.schemas import UserCreate
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_test_settings():
+    settings.configure(FORCE_ENV_FOR_DYNACONF="test")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_up_db():
+    models.Base.metadata.create_all(bind=engine)
+
+
+def test_config_loaded():
+    assert settings.IS_TEST == True
+
+
+def test_create_user():
+    user = create_user_helper("user_1")
+    db = SessionLocal()
+    res = create_user(db, user)
+    assert res.email == "test_1"
+
+
+def test_get_users():
+    user_1 = create_user_helper("user_1")
+    user_2 = create_user_helper("user_2")
+    db = SessionLocal()
+    res_1 = create_user(db, user_1)
+    res_2 = create_user(db, user_2)
+    res_3 = get_users(db)
+    assert len(res_3) == 2
+
+
+def create_user_helper(email):
+    return UserCreate(email=email, password="fake")
